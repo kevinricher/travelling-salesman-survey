a4806d5b17471b1dfc81b74b3c09e101dcc6bf25
MellowMammoth <kevinremiricher@gmail.com>
2020-03-01 21:10:52 -0500

    Started setting up automated testing, set up autocomplete and autoformat.

--- graph_structures.py
+++ graph_structures.py
@@ -2,6 +2,10 @@ class Vertex:
     def __init__(self):
         self.neighbours = []
 
+
 class Graph:
     def __init__(self):
         self.vertices = []
+
+    def add_vertex(self, vertex):
+        self.vertices.append(vertex)
