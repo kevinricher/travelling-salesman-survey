54c9550abb7ad03623501081abf955b071a2cbb9
MellowMammoth <kevinremiricher@gmail.com>
2020-03-04 11:32:22 -0500

    Correcting initial test case, badge switch test.

--- test_graph_structures.py
+++ test_graph_structures.py
@@ -6,4 +6,4 @@ class TestGraphStructures:
         graph = graph_structures.Graph()
         for i in range(3):
             graph.add_vertex(graph_structures.Vertex())
-        assert graph.get_vertex_count() == 2
+        assert graph.get_vertex_count() == 3
