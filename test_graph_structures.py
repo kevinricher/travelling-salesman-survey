2a710dad11238432ce41c40e215d267df5769c46
MellowMammoth <kevinremiricher@gmail.com>
2020-03-04 11:28:15 -0500

    Proving badge changes

--- test_graph_structures.py
+++ test_graph_structures.py
@@ -6,4 +6,4 @@ class TestGraphStructures:
         graph = graph_structures.Graph()
         for i in range(3):
             graph.add_vertex(graph_structures.Vertex())
-        assert graph.get_vertex_count() == 3
+        assert graph.get_vertex_count() == 2
