bc16f7e5b1cec4753f668dc0dc8ca7250e9a228c
krricher <kevin.richer@uwaterloo.ca>
2020-03-14 08:48:53 -0400

    Updated graph_structures.py

--- graph_structures.py
+++ graph_structures.py
@@ -1,11 +1,15 @@
 class Vertex:
     def __init__(self):
-        self.neighbours = []
+        self.neighbours = list()
+        self.visited = False
 
 
 class Graph:
     def __init__(self):
-        self.vertices = []
+        self.vertices = list()
+
+        self.to_search = Stack()
+        self.visited = Set()
 
     def add_vertex(self, vertex):
         self.vertices.append(vertex)
