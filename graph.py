5d3b697f844370b27c18e15fbbffd78924821ff8
krricher <kevin.richer@uwaterloo.ca>
2020-03-17 06:11:40 -0400

    Modified graph.py.

--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
-# import sys
+"""Defines base classes for graph related objects."""
+import sys
+from collections import deque
 
 
 class Vertex:
@@ -10,6 +12,8 @@ class Vertex:
     def __init__(self, vertex_id):
         self.vertex_id = vertex_id
         self.neighbours = dict()
+        self.previous = None
+        self.previous_distance = sys.maxsize
 
     def add_neighbour(self, new_neighbour, weight=0):
         self.neighbours[new_neighbour] = weight
@@ -53,3 +57,31 @@ class Graph:
         if vertex1 in self.vertices and vertex2 in self.vertices:
             vertex1.remove_neighbour(vertex2)
             vertex2.remove_neighbour(vertex1)
+
+
+class ShortestPathFinder:
+    """Function class to find the shortest path using various algorithms."""
+
+    def __init__(self, graph):
+        self.graph = graph
+        self.visit_queue = deque
+        self.visited = set()
+
+    def breadth_first_search(self, vertex1, vertex2):
+        """Searches for the shortest path between vertex1 and vertex2.
+
+        Input: Vertices in self.graph vertex1 and vertex2.
+        Output: List of vertices
+        """
+        # Make sure structures empty.
+        self.visited.clear()
+        self.visit_queue.clear()
+
+        # Initialize and run BFS.
+        self.visit_queue.append(vertex1)
+        self.visited.add(vertex1)
+        while(self.visit_queue):
+            for neighbour in self.visit_queue[-1]:
+                if neighbour not in self.visited:
+                    self.visit_queue.append(neighbour)
+                    self.visited.add(neighbour)
