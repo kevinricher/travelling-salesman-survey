4831b6ab402cc0a6010769a0e3309b8b1b6e50a1
krricher <kevin.richer@uwaterloo.ca>
2020-04-07 15:49:41 -0400

    Hello laptop

--- travelling_salesman.py
+++ travelling_salesman.py
@@ -5,37 +5,28 @@ import time
 
 def main():
     print("Running main:")
-    solver = TSP_Solver(100, 9)
+    solver = TSP_Solver(100, 10)
     t0 = time.clock()
     brute_force_solution = solver.brute_force()
     t1 = time.clock()
     brute_force_solution_length = TSP_Solver.tour_length(brute_force_solution)
-    print(brute_force_solution)
-    print(brute_force_solution_length)
-    print("Time 1: {}, time 2: {}".format(t0, t1))
+    print("Brute force solution: {}.".format(brute_force_solution))
+    print("Brute force solution length: {}.".format(brute_force_solution_length))
     print("Found tour by brute force in {:.3f} seconds.".format(t1-t0))
     # plot_tour(brute_force_solution)
-
-    print(type(set(solver.instance.points)))
-    print(type({solver.first(solver.instance.points)}))
-    print(type(set(solver.instance.points) -
-               {solver.first(solver.instance.points)}))
-    print((set(solver.instance.points) -
-           {solver.first(solver.instance.points)}))
-    print(type(solver.instance.points))
-    print(solver.instance.points)
-    print(type(solver.first(solver.instance.points)))
-    print(type([solver.first(solver.instance.points)]))
 
     t0 = time.clock()
     nn_solution = solver.nearest_neighbour()
     t1 = time.clock()
     nn_solution_length = solver.tour_length(nn_solution)
+    print("Nearest neighbour solution: {}.".format(nn_solution))
+    print("Nearest neighbour solution length: {}.".format(
+        nn_solution_length))
     print("Found tour by nearest neighbour in {:.3f} seconds.".format(t1-t0))
-    plot_tour(nn_solution)
+    # plot_tour(nn_solution)
 
 
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     main()
 else:
     print(__name__)
--- tsp_solver.py
+++ tsp_solver.py
@@ -1,5 +1,4 @@
 import itertools
-import tsp_plot
 from point import Point
 from point_grid import PointGrid
 
@@ -9,8 +8,9 @@ class TSP_Solver:
         self.max_coord = max_coord
         self.point_count = point_count
         self.seed = seed
-        self.instance = PointGrid(
-            max_coord=self.max_coord, point_count=self.point_count, seed=self.seed)
+        self.instance = PointGrid(max_coord=self.max_coord,
+                                  point_count=self.point_count, seed=self.seed)
+        print("Instance points: {}.".format(self.instance.points))
 
     @staticmethod
     def tour_length(tour):
@@ -22,16 +22,18 @@ class TSP_Solver:
     def first(collection):
         return next(iter(collection))
 
+    # Brute force methods.
     def get_all_tours(self):
         start = self.first(self.instance.points)
-        print(type(self.instance.points))
-        return [[start] + list(rest) for rest in itertools.permutations({self.instance.points} - {start})]
+        print("First vertex: {}.".format(start))
+        return [[start] + list(rest) for rest in itertools.permutations(frozenset(self.instance.points) - {start})]
 
     def brute_force(self):
         "Check all tours."
-        all_tours = itertools.permutations(self.instance.points)
+        all_tours = self.get_all_tours()
         return min(all_tours, key=self.tour_length)
 
+    # Nearest neighbour methods.
     @staticmethod
     def _find_nearest_neighbour(key_point, points):
         return min(points, key=lambda c: Point.distance(c, key_point))
@@ -40,9 +42,33 @@ class TSP_Solver:
         "Append each nearest city to form a tour."
         start = self.first(self.instance.points)
         tour = [start]
-        unvisited = (frozenset(self.instance.points) - {start})
+        unvisited = set(frozenset(self.instance.points) - {start})
         while unvisited:
             C = self._find_nearest_neighbour(tour[-1], unvisited)
             tour.append(C)
             unvisited.remove(C)
         return tour
+
+    # Segment reversal methods.
+    def reverse_segment_if_better(self, tour, i, j):
+        "If reversing tour[i:j] would make the tour shorter, then do it."
+        # Given tour [...A-B...C-D...], consider reversing B...C to get
+        # [...A-C...B-D...]
+        p1, p2, p3, p4 = tour[i-1], tour[i], tour[j-1], tour[j % len(tour)]
+        # Are old edges (AB + CD) longer than new ones (AC + BD)? If so,
+        # reverse segment.
+        if (Point.distance(p1, p2) + Point.distance(p3, p4) >
+                Point.distance(p1, p3) + Point.distance(p2, p4)):
+            tour[i:j] = reversed(tour[i:j])
+
+    def alter_tour(self, tour):
+        "Try to alter tour for the better by reversing segments."
+        original_length = self.tour_length(tour)
+        for (start, end) in self.all_segments(len(tour)):
+            self.reverse_segment_if_better(tour, start, end)
+            # If we made an improvement, then try again; else stop and return tour.
+            if self.tour_length(tour) < original_length:
+                return self.alter_tour(tour)
+            return tour
+
+    # Christofides algorithm methods.
